c47627319344824265d0ce8c6e5eebf3a592531d
Taylor Laird <taylorolaird@gmail.com>
2021-06-18 16:53:38 -0400

    setting up api atuff and startig to fromat the display stuff

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+
+
+// API KEY pk_287eca80dc0b4a4d9fcd86c268844876
+// create call api function 
+function call_api(finishedAPI) {
+    request('https://cloud.iexapis.com/stable/stock/fb/quote/?token=pk_287eca80dc0b4a4d9fcd86c268844876', { json: true }, (err, res, body) => {
+        if (err) {return console.log(err);}
+        if (res.statusCode === 200){
+            // console.log(body);
+            finishedAPI(body);
+        };
+    });  
+};
+
+
+
+
+
 
 // Set Handlebars Middle ware
 app.engine('handlebars', exphbs());
@@ -14,9 +33,17 @@ const otherStuff = "hello there, this is other stuff!"
 
 // Set handlebars routes
 app.get('/', function (req, res) {
-    res.render('home', {
-    	stuff: otherStuff
+    call_api(function(doneAPI) {
+            res.render('home', {
+            stock: doneAPI
+        });
     });
+
+});
+
+// Set about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // Set static folder
