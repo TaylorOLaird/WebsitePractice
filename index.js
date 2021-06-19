93bed69f790c3235ecb0a47b337c9ddf7ee8dfb9
Taylor Laird <taylorolaird@gmail.com>
2021-06-19 15:32:24 -0400

    last tweeks before moving to pushing it to heroku

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
 
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
 // API KEY pk_287eca80dc0b4a4d9fcd86c268844876
 // create call api function 
-function call_api(finishedAPI) {
-    request('https://cloud.iexapis.com/stable/stock/fb/quote/?token=pk_287eca80dc0b4a4d9fcd86c268844876', { json: true }, (err, res, body) => {
+function call_api(finishedAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote/?token=pk_287eca80dc0b4a4d9fcd86c268844876', { json: true }, (err, res, body) => {
         if (err) {return console.log(err);}
         if (res.statusCode === 200){
             // console.log(body);
@@ -31,13 +35,29 @@ app.set('view engine', 'handlebars');
 
 const otherStuff = "hello there, this is other stuff!"
 
-// Set handlebars routes
+// Set handlebars index GET route
 app.get('/', function (req, res) {
     call_api(function(doneAPI) {
             res.render('home', {
             stock: doneAPI
         });
-    });
+    }, 'fb');
+
+});
+
+// // Set home page route
+// app.get('/homepage.html', function (req, res) {
+// });
+
+
+// Set handlebars index POST route
+app.post('/', function (req, res) {
+    call_api(function(doneAPI) {
+            // posted_stuff = req.body.stock_ticker;
+            res.render('home', {
+            stock: doneAPI,
+        });
+    }, req.body.stock_ticker);
 
 });
 
